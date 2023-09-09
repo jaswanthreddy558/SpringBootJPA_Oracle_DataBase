a54dddb785c177957e55b331db082e1324e40b09
Jaswanthreddy454@gmail.com <@11K81e0019>
2023-09-09 19:17:26 -0400

    Secutity changes

---
+++ src/main/java/springbootoracledatabase/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package springbootoracledatabase.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+}
