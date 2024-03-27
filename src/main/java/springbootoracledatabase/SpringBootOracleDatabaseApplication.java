2be14e3f37acd267a26d84b84f9387425fce1eff
Jaswanth Kesar <Jaswanth Reddy>
2024-03-27 12:13:03 -0400

    Added new changes

--- src/main/java/springbootoracledatabase/SpringBootOracleDatabaseApplication.java
+++ src/main/java/springbootoracledatabase/SpringBootOracleDatabaseApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 @SpringBootApplication
 @EnableSwagger2
 public class SpringBootOracleDatabaseApplication {
@@ -14,12 +11,7 @@ public class SpringBootOracleDatabaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootOracleDatabaseApplication.class, args);
 
-		try {
-			InetAddress localHost = InetAddress.getLocalHost();
-			System.out.println("Local IP Address: " + localHost.getHostAddress());
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
+
 	}
 
 }
